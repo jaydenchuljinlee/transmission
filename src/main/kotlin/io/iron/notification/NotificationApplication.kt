ed0ae82ab108d6a9c17eac2ee6e95be4cb1b9b08
cheoljin <cheoljin1234@naver.com>
2024-07-25 17:41:16 +0900

    Added: [프로젝트 Initialization] - 초기 프로젝트 설정 파일 추가

---
+++ src/main/kotlin/io/iron/notification/NotificationApplication.kt
@@ -0,0 +1,11 @@
+package io.iron.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationApplication>(*args)
+}
